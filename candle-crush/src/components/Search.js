4691444451e74fcfff56eb61bd22f40f29ce0ed5
sdonohue0918 <sdonohue0918@gmail.com>
2020-11-05 14:42:33 -0500

    added search and filter for scents, 2:41

--- candle-crush/src/components/Search.js
+++ candle-crush/src/components/Search.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
 
-const Search = props => {
+const Search = (props) => {
   return (
-    <TextField id="standard-basic" label="Search" />
+    <TextField id="standard-basic" name="searchValue" label="Search" value={props.searchValue} onChange={props.searchHandler}/>
     )
-}
+
+} 
+  
+
+
+
+
+  
 
 export default Search
